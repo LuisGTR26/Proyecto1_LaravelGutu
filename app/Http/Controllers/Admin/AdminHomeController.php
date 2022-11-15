199bb1d837cda0997717c9b49080b80692fdf860
LuisGTR26 <luis.gutu2601@gmail.com>
2022-11-15 10:41:11 -0500

    primer commit

---
+++ app/Http/Controllers/Admin/AdminHomeController.php
@@ -0,0 +1,12 @@
+<?php
+namespace App\Http\Controllers\Admin;
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class AdminHomeController extends Controller {
+    public function index() {
+        $viewData = [];
+        $viewData["title"] = "Admin Page - Admin - Online Store";
+        return view('admin.home.index')->with("viewData", $viewData);
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -15,3 +15,19 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', 'App\Http\Controllers\HomeController@index')->name("home.index");
 Route::get('/about', 'App\Http\Controllers\HomeController@about')->name("home.about");
+//Lista de todos los productos
+Route::get('/products', 'App\Http\Controllers\ProductController@index')->name("product.index");
+//Vista detalle de producto
+Route::get('/products/{id}', 'App\Http\Controllers\ProductController@show')->name("product.show");
+//Panel home de admin
+Route::get('/admin', 'App\Http\Controllers\Admin\AdminHomeController@index')->name("admin.home.index");
+//Panel de lista de productos
+Route::get('/admin/products', 'App\Http\Controllers\Admin\AdminProductController@index')->name("admin.product.index");
+//Crear productos
+Route::post('/admin/products/store', 'App\Http\Controllers\Admin\AdminProductController@store')->name("admin.product.store");
+//Eliminar producto
+Route::delete('/admin/products/{id}/delete', 'App\Http\Controllers\Admin\AdminProductController@delete')->name("admin.product.delete");
+//Editar producto
+Route::get('/admin/products/{id}/edit', 'App\Http\Controllers\Admin\AdminProductController@edit')->name("admin.product.edit");
+Route::put('/admin/products/{id}/update', 'App\Http\Controllers\Admin\AdminProductController@update')->name("admin.product.update");
+
